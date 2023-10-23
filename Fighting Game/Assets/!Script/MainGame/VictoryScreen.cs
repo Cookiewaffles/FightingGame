6f492c0575f3456f3fcc1cc9899a6d1560a1291a
Cookiewaffles <71943825+Cookiewaffles@users.noreply.github.com>
2023-10-22 23:55:08 -0400

    Menu Update

--- Fighting Game/Assets/!Script/MainGame/VictoryScreen.cs
+++ Fighting Game/Assets/!Script/MainGame/VictoryScreen.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class VictoryScreen : MonoBehaviour
 {
     public GameObject player1WinText;
     public GameObject player2WinText;
+
+
+    public GameObject buttonPLayer1;
+    public GameObject buttonPLayer2;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +27,15 @@ public class VictoryScreen : MonoBehaviour
         {
             player1WinText.SetActive(true);
             player2WinText.SetActive(false);
+
+            EventSystem.current.SetSelectedGameObject(buttonPLayer1);
         }
         else if(player == 2) {
             player1WinText.SetActive(false);
             player2WinText.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(buttonPLayer2);
+
         }
-    
+
     }
 }
